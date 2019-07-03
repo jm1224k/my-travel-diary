614b25182e5bd41b23f0db0114666deec078e461
jm1224k <jm1224k@gmail.com>
2019-07-03 16:46:46 +0900

    coding the ui part 1

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform
+} from "react-native";
+
+const { height, width } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>여행 일지</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input}>여행 일정을 추가해 주세요</TextInput>
+      </View>
     </View>
   );
 }
@@ -12,8 +26,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white"
   },
+  title: {
+    fontSize: 30,
+    marginTop: 50,
+    marginLeft: 50,
+    marginBottom: 30,
+    fontWeight: "400"
+  },
+  input: {
+    color: "#535353",
+    alignContent: "center",
+    paddingTop: 80,
+    fontWeight: "300",
+    fontSize: 20
+  },
+  card: {
+    backgroundColor: "#ececec",
+    alignItems: "center",
+    fontSize: 20,
+    width: width - 40,
+    height: 180,
+    marginLeft: 20,
+    borderRadius: 5,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
 });
