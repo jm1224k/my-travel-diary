6e694206b56b6bc2a10d589800f615d3e41eba6a
jm1224k <jm1224k@gmail.com>
2019-07-09 18:12:06 +0900

    latest

--- App.js
+++ App.js
@@ -9,16 +9,26 @@ import {
   Platform,
   ScrollView
 } from "react-native";
+import { AppLoading } from "expo";
 import Trip from "./trip";
+import { uuidv1 } from "uuid/v1";
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   state = {
-    newTrip: ""
+    newTrip: "",
+    loadedTrip: false,
+    trips: {}
+  };
+  componentDidMount = () => {
+    this._loadTrip();
   };
   render() {
-    const { newTrip } = this.state;
+    const { newTrip, loadedTrip, trips } = this.state;
+    if (!loadedTrip) {
+      return <AppLoading />;
+    }
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
@@ -31,9 +41,12 @@ export default class App extends React.Component {
             onChangeText={this._controlNewTrip}
             placeholderTextColor={"#999"}
             returnKeyType={"done"}
+            onSubmitEditing={this._addTrip}
           />
           <ScrollView contentContainerStyle={styles.trip}>
-            <Trip text={"Ongoing trip"} />
+            {Object.values(trips).map(trip => (
+              <Trip key={trip.id} {...trip} deleteTrip={this._deleteTrip} />
+            ))}
           </ScrollView>
         </View>
       </View>
@@ -42,6 +55,48 @@ export default class App extends React.Component {
   _controlNewTrip = text => {
     this.setState({
       newTrip: text
+    });
+  };
+  _loadTrip = () => {
+    this.setState({
+      loadedTrip: true
+    });
+  };
+  _addTrip = () => {
+    const { newTrip } = this.state;
+    if (newTrip !== "") {
+      this.setState(prevState => {
+        const uuidv1 = require("uuid/v1");
+        const ID = uuidv1();
+        const newTripObject = {
+          [ID]: {
+            id: ID,
+            isCompleted: false,
+            text: newTrip,
+            createdAt: Date.now()
+          }
+        };
+        const newState = {
+          ...prevState,
+          newTrip: "",
+          trips: {
+            ...prevState.trips,
+            ...newTripObject
+          }
+        };
+        return { ...newState };
+      });
+    }
+  };
+  _deleteTrip = id => {
+    this.setState(prevState => {
+      const trips = prevState.trips;
+      delete trips[id];
+      const newState = {
+        ...prevState,
+        ...trips
+      };
+      return { ...newState };
     });
   };
 }
--- trip.js
+++ trip.js
@@ -4,19 +4,30 @@ import {
   Text,
   TouchableOpacity,
   StyleSheet,
-  Dimensions
+  Dimensions,
+  TextInput
 } from "react-native";
+import PropTypes from "prop-types";
 
 const { height, width } = Dimensions.get("window");
 export default class Trip extends Component {
-  state = {
-    isEditing: false,
-    isCompleted: false,
-    tripValue: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      tripValue: props.text
+    };
+  }
+  static PropTypes = {
+    text: PropTypes.string.isRequired,
+    isCompleted: PropTypes.bool.isRequired,
+    deleteTrip: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired
   };
+
   render() {
-    const { isCompleted, isEditing } = this.state;
-    const { text } = this.props;
+    const { isEditing, tripValue } = this.state;
+    const { text, id, deleteTrip, isCompleted } = this.props;
     return (
       <View style={styles.container}>
         <View style={styles.column}>
@@ -28,14 +39,29 @@ export default class Trip extends Component {
               ]}
             />
           </TouchableOpacity>
-          <Text
-            style={[
-              styles.text,
-              isCompleted ? styles.completedText : styles.uncompletedText
-            ]}
-          >
-            {text}
-          </Text>
+          {isEditing ? (
+            <TextInput
+              style={[
+                styles.text,
+                styles.input,
+                isCompleted ? styles.completedText : styles.uncompletedText
+              ]}
+              value={tripValue}
+              multiline={true}
+              onChangeText={this._controlInput}
+              returnKeyType={"done"}
+              onBlur={this._finishEditing}
+            />
+          ) : (
+            <Text
+              style={[
+                styles.text,
+                isCompleted ? styles.completedText : styles.uncompletedText
+              ]}
+            >
+              {text}
+            </Text>
+          )}
         </View>
 
         {isEditing ? (
@@ -53,7 +79,7 @@ export default class Trip extends Component {
                 <Text style={styles.actionText}>🔍</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPressOut={() => deleteTrip(id)}>
               <View style={styles.actionContainer}>
                 <Text style={styles.actionText}>❌</Text>
               </View>
@@ -72,15 +98,18 @@ export default class Trip extends Component {
     });
   };
   _startEditing = () => {
-    const { text } = this.props;
     this.setState({
-      isEditing: true,
-      tripValue: text
+      isEditing: true
     });
   };
   _finishEditing = () => {
     this.setState({
       isEditing: false
+    });
+  };
+  _controlInput = text => {
+    this.setState({
+      tripValue: text
     });
   };
 }
@@ -95,10 +124,10 @@ const styles = StyleSheet.create({
     justifyContent: "space-between"
   },
   circle: {
-    width: 35,
-    height: 35,
-    borderRadius: 100,
-    borderWidth: 5,
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 3,
     marginRight: 30
   },
   completedCircle: {
@@ -121,8 +150,7 @@ const styles = StyleSheet.create({
   column: {
     flexDirection: "row",
     alignItems: "center",
-    width: width / 2,
-    justifyContent: "space-between"
+    width: width / 2
   },
   actions: {
     flexDirection: "row"
@@ -130,5 +158,10 @@ const styles = StyleSheet.create({
   actionContainer: {
     marginVertical: 10,
     marginHorizontal: 10
+  },
+  input: {
+    paddingBottom: 5,
+    marginVertical: 15,
+    width: width / 2
   }
 });
